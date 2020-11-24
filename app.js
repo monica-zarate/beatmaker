beb125f8c0ae2424cb368294bcf2069f63cd1ca9
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-11-24 14:35:38 -0800

    Added the playTrack keyframe and using if statements checked for active pads to play their sounds. Animated the pads.

--- app.js
+++ app.js
@@ -8,15 +8,37 @@ class DrumKit {
     this.index = 0;
     this.bpm = 150;
   }
+  //Toggle active class to selected Pad
   activePad() {
     this.classList.toggle("active");
   }
+  //Repeat method to go over all pads
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(step);
+    //Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      //Check if pads are active
+      if (bar.classList.contains("active")) {
+        //Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
+  //Start method to define beats per minute and call repeat method
   start() {
     const interval = (60 / this.bpm) * 1000;
     setInterval(() => {
@@ -27,10 +49,16 @@ class DrumKit {
 
 const drumKit = new DrumKit();
 
+//Selecting the active pad by clicking on it.
+//Remove the animation to a pad once it gets looped
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
+//Start playing when Play button gets clicked
 drumKit.playBtn.addEventListener("click", () => {
   drumKit.start();
 });
